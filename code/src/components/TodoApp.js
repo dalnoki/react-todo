abc98e226c172b83d6c375409320f87b9d9f589f
ZsPapp <zsofia.papp@sentry.io>
2024-03-01 19:29:09 +0100

    complete css variables

--- code/src/components/TodoApp.js
+++ code/src/components/TodoApp.js
@@ -1,10 +1,11 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 import "../scss/main.scss";
-import Todo from "../components/Todo";
 import AddNewTodo from "./AddNewTodo";
 import Header from "./Header";
 import Footer from "./Footer";
+import TodoList from "./TodoList";
+import DragAndDrop from "./DragAndDrop";
 
 export default function TodoApp() {
   const [allTodos, setAllTodos] = useState([
@@ -43,27 +44,6 @@ export default function TodoApp() {
   ]);
   const [currentTheme, setCurrentTheme] = useState("light");
 
-  const dragItem = useRef();
-  const dragOverItem = useRef();
-
-  const dragStart = (e, index) => {
-    dragItem.current = index;
-  };
-
-  const dragEnter = (e, index) => {
-    dragOverItem.current = index;
-  };
-
-  const drop = () => {
-    const copyListItems = [...filteredTodos];
-    const dragItemContent = copyListItems[dragItem.current];
-    copyListItems.splice(dragItem.current, 1);
-    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
-    dragItem.current = null;
-    dragOverItem.current = null;
-    setFilteredTodos(copyListItems); // Update the state with the reordered list
-  };
-
   const handleThemeChange = () => {
     setCurrentTheme(currentTheme === "light" ? "dark" : "light");
   };
@@ -87,62 +67,20 @@ export default function TodoApp() {
           setFilteredTodos={setFilteredTodos}
           currentTheme={currentTheme}
         />
-        <ul className="todo-list">
-          {filteredTodos.map((currentTodo, index) => {
-            return (
-              <li
-                key={currentTodo.id}
-                draggable
-                onDragStart={(e) => dragStart(e, index)}
-                onDragEnter={(e) => dragEnter(e, index)}
-                onDragOver={(e) => e.preventDefault()}
-                onDrop={drop}
-                className={`${
-                  currentTodo.isCompleted === true
-                    ? "todo-item--completed"
-                    : null
-                } todo-item ${
-                  currentTheme === "light"
-                    ? "todo-item--light"
-                    : "todo-item--dark"
-                } ${
-                  currentTodo.isCompleted && currentTheme === "light"
-                    ? "todo-item--completed--light"
-                    : ""
-                } 
-                
-                ${
-                  currentTodo.isCompleted && currentTheme === "dark"
-                    ? "todo-item--completed--dark"
-                    : ""
-                } round`}
-              >
-                <Todo
-                  todo={currentTodo}
-                  currentTheme={currentTheme}
-                  allTodos={allTodos}
-                  setAllTodos={setAllTodos}
-                  setFilteredTodos={setFilteredTodos}
-                  id={currentTodo.id}
-                  isCompleted={currentTodo.isCompleted}
-                />
-              </li>
-            );
-          })}
-        </ul>
+        <TodoList
+          filteredTodos={filteredTodos}
+          currentTheme={currentTheme}
+          allTodos={allTodos}
+          setAllTodos={setAllTodos}
+          setFilteredTodos={setFilteredTodos}
+        />
         <Footer
           allTodos={allTodos}
           setAllTodos={setAllTodos}
           setFilteredTodos={setFilteredTodos}
           currentTheme={currentTheme}
         />
-        <p
-          className={`dad-info ${
-            currentTheme === "light" ? "dad-info--light" : "dad-info--dark"
-          }`}
-        >
-          Drag and drop to reorder list
-        </p>
+        <DragAndDrop currentTheme={currentTheme} />
       </div>
     </div>
   );
